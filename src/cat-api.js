692f82fd38d6d09f529490818785b6642ddce807
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-08-09 19:15:50 +0300

    change 2

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,15 +1,32 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { URL, refs } from './index.js';
-// import axios from 'axios';
-// axios.defaults.headers.common['x-api-key'] = API_KEY;
+import { refs } from './index.js';
+import axios from 'axios';
 
-export default function fetchBreeds() {
+const URL = 'https://api.thecatapi.com/v1/';
+const API_KEY =
+  'live_Utq0hlMivbHo39VL70zl84zHokxz90UZKgltekVoHuKpuxSnkDAmT7xUyS5OqyVj';
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+function fetchBreeds() {
   const BREEDS_URL = `${URL}breeds`;
-  return fetch(BREEDS_URL).then(res => {
-    if (!res.ok) {
+  return axios.get(BREEDS_URL).then(res => {
+    if (res.status !== 200) {
       throw new Error(Notify.failure(refs.err.textContent));
     }
 
-    return res.json();
+    return res;
   });
 }
+function fetchCatByBreed(breedId) {
+  const IMAGES_URL = `${URL}images/search`;
+  const params = new URLSearchParams({
+    breed_ids: breedId,
+  });
+  return axios.get(`${IMAGES_URL}?${params}`).then(res => {
+    if (res.status !== 200) {
+      throw new Error(Notify.failure(refs.err.textContent));
+    }
+    return res.data[0];
+  });
+}
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-export { URL, refs };
-
-import axios from 'axios';
 import SlimSelect from 'slim-select';
-import fetchBreeds from './cat-api.js';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'slim-select/dist/slimselect.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-const URL = 'https://api.thecatapi.com/v1/';
 const refs = {
   select: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
   err: document.querySelector('.error'),
+  catCard: document.querySelector('.cat-info'),
 };
-const API_KEY =
-  'live_Utq0hlMivbHo39VL70zl84zHokxz90UZKgltekVoHuKpuxSnkDAmT7xUyS5OqyVj';
-axios.defaults.headers.common['x-api-key'] = API_KEY;
 
-// refs.loader.textContent = '';
-refs.select.style.display = 'none';
 refs.err.style.display = 'none';
+refs.select.style.display = 'none';
 
-// fetchBreeds().then(data => console.log(data));
 fetchBreeds()
   .then(data => {
     refs.select.style.display = 'block';
@@ -42,21 +33,10 @@ function createMarkupOptins(arr) {
 
 refs.select.addEventListener('change', e => {
   const id = e.target.value;
-  console.log(id);
+  //   console.log(id);
   fetchCatByBreed(id)
-    .then(data => console.log(data))
+    .then(catInfo => console.log(dacatInfota))
     .catch(err => err);
 });
 
-function fetchCatByBreed(breedId) {
-  const IMAGES_URL = `${URL}images/search`;
-  const params = new URLSearchParams({
-    breed_ids: breedId,
-  });
-  return axios.get(`${IMAGES_URL}?${params}`).then(res => {
-    if (!res.ok) {
-      throw new Error(Notify.failure(refs.err.textContent));
-    }
-    return res.json();
-  });
-}
+export { refs };
